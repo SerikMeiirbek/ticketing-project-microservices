701ff0656fc9f30c772596218a732a606864020d
serikmeiirbek <serik.meiirbek@gmail.com>
2023-04-19 17:16:08 -0400

    OpenAPI

--- gateway-service/src/main/java/com/cydeo/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/cydeo/GatewayServiceApplication.java
@@ -1,8 +1,16 @@
 package com.cydeo;
 
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +20,21 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
 
     }
+
+    @Bean
+    public List<GroupedOpenApi> apiList(SwaggerUiConfigParameters swaggerUiConfigParameters,
+                                        RouteDefinitionLocator routeDefinitionLocator){
+        List<GroupedOpenApi> groupedOpenApis = new ArrayList<>();
+        List<RouteDefinition> definitions = routeDefinitionLocator
+                .getRouteDefinitions().collectList().block();
+
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")
+        ).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("-service","");
+            swaggerUiConfigParameters.addGroup(name);
+            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+        });
+        return groupedOpenApis;
+    }
+
 }
